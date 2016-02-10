20fd88500253c6949d52a0ce6478d4b3a48ae27d
adam-rabinowitz <adamrabs@hotmail.com>
2016-02-10 15:28:35 +0000

    Added scde function

--- RNASeq.R
+++ RNASeq.R
@@ -1,7 +1,7 @@
 ##############################################################################
 ## filter RNA-Seq counts
 ##############################################################################
-filterRNACounts <- function(tpm, exp, reads=400000, genes=2000, mintpm=10,
+filterTPMCounts <- function(tpm, exp, reads=400000, genes=2000, mintpm=10,
   minratio=0.1, minsample=5) {
   # Read in data if files supplied
   if (is.character(tpm)) {
@@ -37,6 +37,59 @@ filterRNACounts <- function(tpm, exp, reads=400000, genes=2000, mintpm=10,
 }
 
 ##############################################################################
+## Filter Exp counts
+###############################################################################
+filterExpCounts <- function(tpm, exp, reads=400000, genes=2000, mintpm=10,
+  minratio=0.1, minsample=5) {
+  # Read in data if files supplied
+  if (is.character(tpm)) {
+    message('Reading TPM count file')
+    tpm = read.table(tpm, sep='\t', header=T, row.names=1)
+  }
+  if (is.character(exp)) {
+    message('Reading expected count file')
+    exp = read.table(exp, sep='\t', header=T, row.names=1)
+  }
+  # Extract samples with sufficient aligned reads and expressed genes
+  acceptedSamples <- colnames(exp)[
+    colSums(exp) >= reads &
+      apply(exp, 2, function(z) {sum(z > 0)}) >= genes
+    ]
+  message(paste(length(acceptedSamples), 'of', ncol(tpm),
+    'samples accepted'))
+  # Calculate number of samples in which a gene must be expresed
+  sampleNo <- ceiling(max(minsample, length(acceptedSamples) * minratio))
+  message(paste('Finding Genes expressed in >=', sampleNo, 'samples'))
+  # Extract accepted genes
+  acceptedGenes <- row.names(tpm)[
+    apply(
+      tpm[,acceptedSamples], 1, function(z) {sum(z >= mintpm)}
+    ) >= sampleNo
+    ]
+  message(paste(length(acceptedGenes), 'of', nrow(tpm),
+    'genes accepted'))
+  # Extract and return filtered counts as matrix
+  filteredExp <- exp[acceptedGenes,acceptedSamples]
+  filteredExp <- as.matrix(filteredExp)
+  return(filteredExp)
+}
+
+##############################################################################
+## Create gene translation vecotr
+##############################################################################
+# Extract file for gene name conversion
+createTran <- function(tranfile) {
+  # Read in tran data
+  tranData <- read.table(tranfile, sep = '\t', header = T,
+    stringsAsFactors=F)
+  # Split alternative names by source name
+  tranData <- split(tranData[,2], tranData[,1])
+  # Create character vecor and return
+  tranData <- sapply(tranData, paste0, collapse=' ')
+  return(tranData)
+}
+
+##############################################################################
 ## Check concordane between samples and group list
 ##############################################################################
 checkGroups <- function(samples, groups) {
@@ -59,3 +112,65 @@ checkGroups <- function(samples, groups) {
     return(g)
   })
 }
+
+###############################################################################
+## Function to perform scde analysis
+###############################################################################
+performSCDE <- function(counts, groups, altname=NULL, cores=4) {
+  # Load required module
+  require('scde')
+  # Check groups
+  groups <- checkGroups(colnames(counts), groups)
+  # Check that each group has two factors
+  factorCount <- sapply(groups, function(z) {length(levels(z))})
+  countTable <- table(factorCount)
+  if (countTable['2'] != sum(countTable)) {
+    stop('groups must have exactly two factors')
+  }
+  # Round counts
+  counts <- round(counts,digits=0)
+  storage.mode(counts) <- 'integer'
+  # Perform scde
+  results <- lapply(groups, function(z){
+    # Build models
+    scde.fitted.model <- scde.error.models(
+      counts=counts,
+      n.cores=cores,
+      save.model.plots=F
+    )
+    scde.prior <- scde.expression.prior(
+      models=scde.fitted.model,
+      counts=counts
+    )
+    # Calculate differential expression
+    ediff <- scde.expression.difference(
+      scde.fitted.model,
+      counts,
+      scde.prior,
+      groups = groups[[1]],
+      n.cores = 4
+    )
+    # Calculate p-values
+    ediff$pvalue <- 2*pnorm(abs(ediff$Z),lower.tail=F) # 2-tailed p-value
+    ediff$padj <- p.adjust(ediff$pvalue,'BH')
+    ediff <- ediff[order(ediff$padj),]
+    # Add gene names and adjust column order
+    ediff$gene <- row.names(ediff)
+    if (!is.null(altname)) {
+      ediff$altname <- altname[ediff$gene]
+      ediff <- ediff[,c(9:10,1:8)]
+    } else {
+      ediff <- ediff[,c(9,1:8)]
+    }
+    # rename rows and return
+    row.names(ediff) <- 1:nrow(ediff)
+    return(ediff)
+  })
+  return(results)
+}
+
+
+
+
+
+
