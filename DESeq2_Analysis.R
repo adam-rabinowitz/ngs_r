9cdc5dbd5bd6c07e58cde4e5af8332a8ca6ab156
adam-rabinowitz <adamrabs@hotmail.com>
2016-05-19 09:20:46 +0100

    Modified script to output biotype data if required

--- DESeq2_Analysis.R
+++ DESeq2_Analysis.R
@@ -11,19 +11,15 @@ option_list = list(
               type='character', help='output prefix'),
   make_option(c("-B", "--modelBatch"), action="store_true", default=F,
               type='logical', help='add batch effects to model [FALSE]'),
-  make_option(c("-A", "--altName"), action="store", default=NA,
+  make_option(c("-N", "--altName"), action="store", default=NA,
               type='character', help='alternative gene name file [NA]'),
+  make_option(c("-T", "--bioType"), action="store", default=NA,
+              type='character', help='gene biotype file [NA]'),
   make_option(c("-P", "--padj"), action="store", default=0.05,
               type='numeric', help='adjusted p-value [0.05]')
 )
 # Import arguments
 opt = parse_args(OptionParser(option_list=option_list))
-#opt$tpmData = '/farm/scratch/rs-bio-lif/rabino01/evaRiising/CD9_Data/CD9_gene.tpm'
-#opt$expData = '/farm/scratch/rs-bio-lif/rabino01/evaRiising/CD9_Data/CD9_gene.exp'
-#opt$sampleData = '/farm/scratch/rs-bio-lif/rabino01/evaRiising/CD9_Data/CD9_sampleData.txt'
-#opt$altName = '/farm/scratch/rs-bio-lif/rabino01/evaRiising/genomeData/mouse_ensembl_2_mouse_mgi.txt'
-#opt$outPrefix = '/home/rabino01/test'
-#opt$modelBatch = TRUE
 # Check required arguments have been supplied
 if (is.na(opt$expData)) {
   stop('Expected count file must be supplied')
@@ -40,18 +36,20 @@ require('ggplot2')
 require('ggdendro')
 # Read sample data file
 sampleData <- read.table(opt$sampleData, sep = '\t', header = T,
-  row.names = 1)
+  row.names = 1, check.names = F)
 sampleData$condition <- factor(sampleData$condition,
   levels=unique(sampleData$condition))
 sampleData$replicate <- factor(sampleData$replicate,
   levels=unique(sampleData$replicate))
 # Read expected count file
 expData <- read.table(opt$expData, sep = '\t', row.names = 1,
-  header = T)
-expData <- expData[,row.names(sampleData)]
-if (ncol(expData) != nrow(sampleData)) {
-  stop('Expected counts not present for all samples')
+  header = T, check.names = 'F')
+# Check column names in expected count file and resort
+inter <- intersect(row.names(sampleData), colnames(expData))
+if (length(inter) != length(row.names(sampleData))) {
+    stop('Not all samples in count data')
 }
+expData <- expData[,row.names(sampleData)]
 # Round expected counts
 expData <- round(expData)
 # Extract file for gene name conversion
@@ -60,6 +58,13 @@ if (!is.na(opt$altName)) {
     stringsAsFactors=F)
   nameData <- split(nameData[,2], nameData[,1])
   nameData <- sapply(nameData, paste0, collapse=' ')
+}
+# Extract file for biotype conversion
+if (!is.na(opt$bioType)) {
+  bioData <- read.table(opt$bioType, sep = '\t', header = T,
+    stringsAsFactors=F)
+  bioData <- split(bioData[,2], bioData[,1])
+  bioData <- sapply(bioData, paste0, collapse=' ')
 }
 # Create DESeq2 dataset
 if (opt$modelBatch) {
@@ -150,23 +155,30 @@ extractResults  <- function(DESeqObject, prefix = opt$outPrefix) {
   # Extract results and sort by p-value
   resultData <- as.data.frame(results(DESeqObject))
   resultData <- resultData[order(resultData$pvalue),]
-  resultData$gene <- row.names(resultData)
+  # Add biotype and reorder
+  if (!is.na(opt$bioType)) {
+    resultData$biotype <- bioData[row.names(resultData)]
+    resultData <- resultData[,c(ncol(resultData),1:(ncol(resultData)-1))]
+  }
   # Add alternative gene names and reorder
   if (!is.na(opt$altName)) {
     resultData$altname <- nameData[row.names(resultData)]
-    resultData <- resultData[,c(7:8,1:6)]
-  } else {
-    resultData <- resultData[,c(7,1:6)]
+    resultData <- resultData[,c(ncol(resultData),1:(ncol(resultData)-1))]
   }
+  # Add gene names to dataframe and reorder
+  resultData$gene <- row.names(resultData)
+  resultData <- resultData[,c(ncol(resultData),1:(ncol(resultData)-1))]
   # Extract and sort counts
-  countData <- counts(DESeqObject, normalized=T)
-  countData <- countData[row.names(resultData),]
+  countData <- as.data.frame(counts(DESeqObject, normalized=T))
+  countData <- countData[resultData$gene,]
+  countData$gene <- row.names(countData)
+  countData <- countData[,c(ncol(countData),1:(ncol(countData)-1))]
   # Save results data
   write.table(resultData, file = paste0(prefix,'.results'), sep = '\t',
     quote = F, col.names = T, row.names = F)
   # Save count data
   write.table(countData, file = paste0(prefix,'.counts'), sep = '\t',
-    quote = F, col.names = NA, row.names = T)
+    quote = F, col.names = T, row.names = F)
 }
 # Store results and count data
 extractResults(DESeqAnalysis)
@@ -177,9 +189,3 @@ print(cluster(DESeqAnalysis))
 print(maPlot(DESeqAnalysis))
 print(volcanoPlot(DESeqAnalysis))
 dev.off()
-
-
-
-
-
-
